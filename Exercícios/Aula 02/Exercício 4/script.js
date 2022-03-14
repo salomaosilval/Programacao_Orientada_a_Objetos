038def2639cf139ae7ff886bf97b90996d196790
Salomão da Silva Leal <salomaosilval@hotmail.com>
2022-03-13 21:34:12 -0400

    Exercício 4 da Aula 02

---
+++ Exercícios/Aula 02/Exercício 4/script.js
@@ -0,0 +1,8 @@
+class CNH {
+    constructor(pais, idade) {
+        this.pais = pais,
+        this.idade = idade
+    }
+}
+
+// NÃO CONSEGUI IMPLEMENTAR O PROBLEMA COMPLETO ENTÃO VOU ESPERAR A CORREÇÃO PRA VER COMO ERA PRA FAZER
