f8852b10faf6b26652fd8cd1102f58737a9c0e35
Salomão da Silva Leal <salomaosilval@hotmail.com>
2022-03-14 20:58:44 -0400

    Correção do Exercício 2.1

--- Exercícios/Aula 02/Exercício 4/script.js
+++ Exercícios/Aula 02/Exercício 4/script.js
@@ -1,8 +1,27 @@
 class CNH {
-    constructor(pais, idade) {
-        this.pais = pais,
-        this.idade = idade
+    constructor(pais, categoria) {
+        this.pais = pais;
+        this.categoria = categoria;
+    }
+        get idade() {
+            if (this.pais == "US" || this.pais == "CA") {
+            let idade = 16;
+            `O país é ${this.pais}, a idade é ${idade}`
+        } else if (this.pais == "CH" || this.pais == "RU") {
+            let idade = 21;
+            `O país é ${this.pais}, a idade é ${idade}`
+        } else if (this.pais == "BR") {
+            if (this.categoria == undefined) {
+                return `Digite a categoria`
+            } else {
+                let idade = 18;
+                return `O país é ${this.pais}, a idade é ${idade} e a categoria é ${this.categoria}`;
+            }
+        } else {
+            return "País não catalogado"
+        }
     }
 }
 
-// NÃO CONSEGUI IMPLEMENTAR O PROBLEMA COMPLETO ENTÃO VOU ESPERAR A CORREÇÃO PRA VER COMO ERA PRA FAZER
+const Carteira = new CNH("BR", "A");
+console.log(Carteira.idade);
